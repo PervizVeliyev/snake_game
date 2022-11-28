b53fab7bad1282505613610c56797331f7c19edf
PervizVeliyev <veliyevperviz1@gmail.com>
2022-11-28 21:44:17 +0400

    first commit

---
+++ src/main/java/com/codenjoy/dojo/snake/client/Solution.java
@@ -0,0 +1,31 @@
+package com.codenjoy.dojo.snake.client;
+
+import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.services.Point;
+
+import java.util.List;
+
+public class Solution {
+    private static final Lee lee = new Lee(15, 15);
+
+    public static Direction solve(Board board){
+        Point apple = board.getApples().get(0);
+        List<Point> obstacles = board.getBarriers();
+        Point start = board.getHead();
+        if(lee.trace(start, apple, obstacles).isPresent()){
+            List<LeePoint> path = lee.trace(start, apple, obstacles).get();
+            LeePoint nextMove = path.size() > 1 ? path.get(1) : new LeePoint(0,0);
+            if(nextMove.y() == start.getY() && nextMove.x() > start.getX()) return Direction.RIGHT;
+            else if(nextMove.y() == start.getY() && nextMove.x() < start.getX()) return Direction.LEFT;
+            else if(nextMove.x() == start.getX() && nextMove.y() < start.getY()) return Direction.DOWN;
+            else return Direction.UP;
+        }
+        else{
+            LeePoint nextMove = lee.nonTarget(start, obstacles);
+            if(nextMove.y() == start.getY() && nextMove.x() > start.getX()) return Direction.RIGHT;
+            else if(nextMove.y() == start.getY() && nextMove.x() < start.getX()) return Direction.LEFT;
+            else if(nextMove.x() == start.getX() && nextMove.y() < start.getY()) return Direction.DOWN;
+            else return Direction.UP;
+        }
+    }
+}
